166fa036bee43301ccf79ba6322e8cad0d46b060
That Lisa <thatlisajones@gmail.com>
2019-01-16 13:14:53 -0700

    Making changes for Heroku

--- config/orm.js
+++ config/orm.js
@@ -29,7 +29,7 @@ function objToSql(ob) {
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {burger_name: 'Lana Del Grey'} => ["burger_name='Lana Del Grey'"]
       // e.g. {sleepy: true} => ["sleepy=true"]
       arr.push(key + "=" + value);
     }
@@ -70,7 +70,7 @@ var orm = {
       cb(result);
     });
   },
-  // An example of objColVals would be {name: panther, sleepy: true}
+  // An example of objColVals would be {burger_name: panther, sleepy: true}
   update: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
--- db/schema.sql
+++ db/schema.sql
@@ -3,10 +3,18 @@
 CREATE DATABASE burger_db;
 USE burger_db;
 
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
+CREATE TABLE burgers (
+	id INT AUTO_INCREMENT NOT NULL,
+	burger_name varchar(225) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
+	createdAt TIMESTAMP NOT NULL,
 	PRIMARY KEY (id)
 );
+
+-- CREATE TABLE burgers
+-- (
+-- 	id int NOT NULL AUTO_INCREMENT,
+-- 	name varchar(255) NOT NULL,
+-- 	devoured BOOLEAN DEFAULT false,
+-- 	PRIMARY KEY (id)
+-- );
